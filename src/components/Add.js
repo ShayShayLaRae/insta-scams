d51b72999d2b9594da9fdad40428e6d0c926b2ab
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-14 11:43:56 -0700

    initial front-end setup

---
+++ src/components/Add.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+export default class Add extends Component {
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+    
+        }
+    }
+   
+    render() {
+        return (
+            <div>
+               
+            </div>
+        )
+    }
+  
+}
---
+++ src/components/Register.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class Register extends Component {
+    constructor() {
+        super();
+        state= {
+            email: '',
+            name: '',
+            password: '',
+            
+        }
+    }
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+  
+}
+
